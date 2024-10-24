7a53ec7c96beb00d0c75d4594ccbbb96ae0473e5
jorge Rosas <jorge.99rg@gmail.com>
2024-10-24 16:28:52 -0600

    fibonacci

---
+++ src/exercises/Fibonacci.java
@@ -0,0 +1,26 @@
+package exercises;
+
+//Simple fibonacci practice
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+
+		int a=0;
+		int b=1;
+		int c=0;
+		
+		int series_length = 20;
+		
+		System.out.print(a+","+b);
+		
+		for(int i=0;i<series_length;i++) {
+			c=a+b;
+			System.out.print(","+c);
+			a=b;
+			b=c;
+		}
+
+	}
+
+}
---
+++ src/exercises/Series0.java
@@ -0,0 +1,8 @@
+package exercises;
+
+public class Series0 {
+	
+	
+	
+	
+}
