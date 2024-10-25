1585d68841415abd4250772b40c4fb83742c2fe0
jorge Rosas <jorge.99rg@gmail.com>
2024-10-25 00:12:39 -0600

    series 0 done.

--- src/exercises/Series0.java
+++ src/exercises/Series0.java
@@ -4,42 +4,52 @@ public class Series0 {
 	
 	public static void main(String args[]) {
 		
-		String mainString = "3122113378";
-		int iterations = 5;
-		int l = 0;
-		int digit = 0;
+		String mainString = "1", auxString="";
+		int iterations = 10;
+		int l;
+		int digit;
 		
 		
 		System.out.println(mainString.length());
 		
-		//for(int i=0;i<iterations;i++) {
+		for(int i=0;i<iterations;i++) {
+			
+			l=0;
+			digit=0;
 			
 			for(int j=0;j<mainString.length();j++) {
 				
 				
 				//System.out.println("\nj: "+j);
-				if (digit==0) {
+				if (j==0) {
 					digit=Integer.parseInt(mainString.substring(0,1));
-					l++;
 					//System.out.println("start");
 				}				
 				
-				else if (digit==Integer.parseInt(mainString.substring(j,j+1))){
+				if (digit==Integer.parseInt(mainString.substring(j,j+1))){
 					l++;
-					
 					//System.out.println("case2");
 				} 
 				else if(digit!=Integer.parseInt(mainString.substring(j,j+1))){
-					System.out.print(l);
-					System.out.print(digit);
+					
+					auxString+=l;
+					auxString+=digit;
 					l=1;
 					digit=Integer.parseInt(mainString.substring(j,j+1));
 				}
 				
+				if(j==mainString.length()-1) {
+					auxString+=l;
+					auxString+=digit;
+				}
+				
 			}
-			System.out.println();
 			
-		//}
+			System.out.println(auxString);
+			mainString = auxString;
+			auxString="";
+			
+		}
 		
 	}
 	
