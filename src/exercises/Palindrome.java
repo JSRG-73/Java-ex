cca5967cf9f14f71ce3ee6557f94657dbfb19013
jorge Rosas <jorge.99rg@gmail.com>
2024-10-28 23:29:59 -0600

    palindrome class started

---
+++ src/exercises/Palindrome.java
@@ -0,0 +1,17 @@
+package exercises;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String sentenceString="palabra";
+		
+		if(sentenceString.length()%2==0) {
+			System.out.println("even");
+		} else {
+			System.out.println("odd");
+		}
+		
+	}
+
+}
