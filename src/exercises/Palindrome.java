749a61950d665c6f9739f18693a9189980d4defd
jorge Rosas <jorge.99rg@gmail.com>
2024-10-29 09:11:07 -0600

    Palindrome class done.

--- src/exercises/Palindrome.java
+++ src/exercises/Palindrome.java
@@ -1,21 +1,55 @@
 package exercises;
+import java.util.Scanner;
 
 public class Palindrome {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String sentenceString="palabras";
+	static String function1(String sentenceString) {
+		
+		sentenceString=sentenceString.replace(" ", "");
+		sentenceString=sentenceString.replace("á", "a");
+		sentenceString=sentenceString.replace("é", "e");
+		sentenceString=sentenceString.replace("í", "i");
+		sentenceString=sentenceString.replace("ó", "o");
+		sentenceString=sentenceString.replace("ú", "u");
+		sentenceString=sentenceString.toLowerCase();
 		
 		if(sentenceString.length()%2==0) {
-			System.out.println("even");
+			
+			//System.out.println("even");
 			for(int i=0;i<(sentenceString.length()/2);i++) {
-				System.out.print(sentenceString.substring(i,i+1));
-				System.out.println(sentenceString.substring(i,i+1));
+				if(sentenceString.substring(i,i+1).equals(sentenceString.substring(sentenceString.length()-i-1,sentenceString.length()-i))) {
+					continue; //System.out.println(true);
+				} else {
+					return "It's not a palindrome"; //System.out.println(false);
+				}
 			}
 		} else {
-			System.out.println("odd");
+			
+			//System.out.println("odd");
+			for(int i=0;i<(sentenceString.length()/2);i++) {
+				if(sentenceString.substring(i,i+1).equals(sentenceString.substring(sentenceString.length()-i-1,sentenceString.length()-i))) {
+					continue; //System.out.println(true);
+				} else {
+					return "It's not a palindrome"; //System.out.println(false);
+				}
+			}
 		}
 		
+		return "It's a palindrome";
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner inScanner = new Scanner(System.in);
+		String sentenceString;//"Sómos o no somos";
+		while(true) {
+			System.out.println("Type your String: ");
+			sentenceString = inScanner.nextLine();
+			System.out.println(function1(sentenceString));
+		}
+
+		//inScanner.close();
 	}
 
 }
