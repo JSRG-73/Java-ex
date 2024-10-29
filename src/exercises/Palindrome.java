e9ecbd0484a60793d159ed8e4a6f24ad51d49519
jorge Rosas <jorge.99rg@gmail.com>
2024-10-28 23:56:08 -0600

    advances in palindrome

--- src/exercises/Palindrome.java
+++ src/exercises/Palindrome.java
@@ -4,10 +4,14 @@ public class Palindrome {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String sentenceString="palabra";
+		String sentenceString="palabras";
 		
 		if(sentenceString.length()%2==0) {
 			System.out.println("even");
+			for(int i=0;i<(sentenceString.length()/2);i++) {
+				System.out.print(sentenceString.substring(i,i+1));
+				System.out.println(sentenceString.substring(i,i+1));
+			}
 		} else {
 			System.out.println("odd");
 		}
