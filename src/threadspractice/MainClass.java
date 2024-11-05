6faf888f62aae3a2666d42c9734d0e75c56e9be4
jorge Rosas <jorge.99rg@gmail.com>
2024-11-04 21:12:38 -0600

    threads practice 2

--- src/threadspractice/MainClass.java
+++ src/threadspractice/MainClass.java
@@ -3,13 +3,20 @@ package threadspractice;
 public class MainClass {
 
 	public static void main(String[] args) {
+		try {
+			Process1 thread1 = new Process1();		
+			Process1 thread2 = new Process1();
+			//Thread thread2 = new Thread(new Process2());
+			
+			thread1.stringsetter("this is the first thread");
+			thread2.stringsetter("this is the second thread");
+			thread1.start();
+			thread2.start();
+		}catch (Exception e) {
+			System.out.println(e);
+		}
 
-		Process1 thread1 = new Process1();		
-		Thread thread2 = new Thread(new Process2());
 		
-		thread1.start();
-		thread2.start();
 		
 	}
-
 }
--- src/threadspractice/Process1.java
+++ src/threadspractice/Process1.java
@@ -1,11 +1,17 @@
 package threadspractice;
 
 public class Process1  extends Thread{
+	
+	private String s;
 
 	@Override
 	public void run() {
 		for(int i=0;i<=5;i++) {
-			System.out.println("process 1");
+			System.out.println(s);
 		}
+	}
+	
+	public void stringsetter(String string) {
+		this.s = string;
 	}
 }
