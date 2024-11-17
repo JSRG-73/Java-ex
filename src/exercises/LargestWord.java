bc50fc07c585563cb1008c1714d69be32719653e
jorge Rosas <jorge.99rg@gmail.com>
2024-11-16 23:40:12 -0600

    Largest word completed, Run-Length Encoding started

--- src/exercises/LargestWord.java
+++ src/exercises/LargestWord.java
@@ -1,11 +1,22 @@
 package exercises;
 
+import java.util.Scanner;
+
 public class LargestWord {
 
 	public static void main(String[] args) {
 		
-		String s = "The quick brown fox jumps over the lazy dog.";
+		Scanner inScanner = new Scanner(System.in);
+		System.out.println("Enter a sentence: ");
+		String string = inScanner.nextLine();	
+		inScanner.close();
 		
+		string = string.replaceAll("!|,|\\.|\\?", "").toLowerCase().trim();
+		String[] s_array = string.split("\\s+");
+		int max=0;
+		
+		for(int i=0; i<s_array.length;i++) if(s_array[i].length()>max) max=s_array[i].length();
+		
+		for(int i=0; i<s_array.length;i++) if(s_array[i].length()==max) System.out.println(s_array[i]);
 	}
-
 }
