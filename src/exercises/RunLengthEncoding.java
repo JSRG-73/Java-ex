8d795c091a87f2f9dd072637a9e30f5dcf94b181
jorge Rosas <jorge.99rg@gmail.com>
2024-11-17 23:29:55 -0600

    RunLengthEncoding completed, ConsecutiveDuplicatesRemover started

--- src/exercises/RunLengthEncoding.java
+++ src/exercises/RunLengthEncoding.java
@@ -3,8 +3,29 @@ package exercises;
 public class RunLengthEncoding {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		String string1 = "aaabbcdddde";
+		String string2 = "ppppqqrrrsssssttttttuuuuvvvwwxxxyyyzzz";
+		String string = "aaaaaaaaaaabbbbbbccccccccccddddddddddddddeeeeeeefffffffffggggggggghhhhhhhhh";
+		
+		Character c=string.charAt(0);
+		int counter=0;
+		StringBuilder sb = new StringBuilder("");
+		
+		if (string.isEmpty()) {
+		    System.out.println("La cadena está vacía");
+		    return;
+		}
+		
+		for (int i=0;i<string.length();i++) {
+			if(c==string.charAt(i)) counter++;
+			else {
+				sb.append(c).append(counter);
+				c=string.charAt(i);
+				counter=1;
+			}
+		}
+		sb.append(c).append(counter);
+		System.out.println(sb);
 	}
-
 }
