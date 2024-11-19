7b76fb12e751e8cf9228f1877e28c185ef67f8e3
jorge Rosas <jorge.99rg@gmail.com>
2024-11-18 23:39:24 -0600

    ConsecutiveDuplicatesRemover done

--- src/exercises/ConsecutiveDuplicatesRemover.java
+++ src/exercises/ConsecutiveDuplicatesRemover.java
@@ -4,6 +4,22 @@ public class ConsecutiveDuplicatesRemover {
 
 	public static void main(String[] args) {
 		
-
+		 if (s.isEmpty()) {
+	            System.out.println("The string is empty.");
+	            return;
+	        }
+		
+		String s = "aaabbccddeeffgghhh";
+		char c = s.charAt(0);
+		StringBuilder sb = new StringBuilder("");
+		sb.append(c);
+		
+		for (int i=0; i<s.length();i++) {
+			if (s.charAt(i)!=c) {
+				c=s.charAt(i);
+				sb.append(c);
+			}
+		}
+		System.out.println(sb);
 	}
 }
