e1fcf13064f20a1937398ee221632eb6aec402cc
jorge Rosas <jorge.99rg@gmail.com>
2024-11-25 23:31:56 -0600

    Streams testing

--- src/exercises/StreamTest.java
+++ src/exercises/StreamTest.java
@@ -1,11 +1,24 @@
 package exercises;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+
 public class StreamTest {
 
 	public static void main(String[] args) {
 
+		ArrayList<String> al = new ArrayList<String>();
+		al.add("USA");
+		al.add("France");
+		al.add("Mexico");
+		al.add("Mexico");
+		al.add("Mexico");
+		al.add("Morocco");
 
-
+		
+		System.out.println(al.stream().distinct());
 	}
 
 }
