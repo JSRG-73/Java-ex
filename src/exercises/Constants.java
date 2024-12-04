6b149721468e2f23ce4f393afd476316854d4fa3
jorge Rosas <jorge.99rg@gmail.com>
2024-12-03 20:01:41 -0600

    MAny classes created and updated

--- src/exercises/Constants.java
+++ src/exercises/Constants.java
@@ -1,10 +1,11 @@
-
 package exercises;
 
-public class Generics {
+public class Constants {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("e: " + Math.E);
 
 	}
 
---
+++ src/exercises/Random.java
@@ -0,0 +1,13 @@
+package exercises;
+
+public class Random {
+	
+	public static void main(String args[]) {
+
+		int r = (int) (Math.random() * 1000);
+		
+		System.out.println(r);
+		
+	}
+
+}
