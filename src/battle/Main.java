5bffd77582980fa35b35586f73bf4dc45adae9d3
jorge Rosas <jorge.99rg@gmail.com>
2024-12-12 23:24:51 -0600

    BS updated

--- src/battle/Main.java
+++ src/battle/Main.java
@@ -2,10 +2,10 @@ package battle;
 
 public class Main {
 	
-	public Creature round(Creature attacker, Creature defender) {
+	public String round(Creature attacker, Creature defender) {
 		int randomNum = (int)(Math.random() * (attacker.skill + defender.skill));
-		if (randomNum<=attacker.skill) return attacker;
-		else return defender;
+		if (randomNum<=attacker.skill) return attacker.name;
+		else return defender.name;
 	}
 	
 	public boolean dodge(Creature c) {
@@ -17,6 +17,11 @@ public class Main {
 	public static void main(String[] args) {
 
 		Creature c1 = new Creature("dragon","d1",100,30,72,10);
+		Creature c2 = new Creature("dragon","d2",100,30,72,10);
+		
+		Main m = new Main();
+
+		System.out.println(m.round(c1, c2));
 
 	}
 
