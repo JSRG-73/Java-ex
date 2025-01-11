612b7430e2e7a01a1cd386ea240f82bb4fd62429
jorge Rosas <jorge.99rg@gmail.com>
2025-01-10 22:50:37 -0600

    Fixed mistakes in PasswordStrenghtEvaluator and added common cases management

--- src/exercises/PasswordStrengthEvaluator.java
+++ src/exercises/PasswordStrengthEvaluator.java
@@ -4,6 +4,29 @@ import java.util.Scanner;
 
 public class PasswordStrengthEvaluator {
 	
+	private static boolean isSequential(String ps) {
+        for (int i = 0; i < ps.length() - 1; i++) {
+            if (ps.charAt(i) + 1 == ps.charAt(i + 1)) return true;
+            if (ps.charAt(i) - 1 == ps.charAt(i + 1)) return true; 
+        }
+        return false;
+    }
+
+    private static boolean isRepeatedPattern(String ps) {
+        String pattern = ps.substring(0, ps.length() / 2);
+        if (ps.length() % pattern.length() == 0 && ps.replace(pattern, "").isEmpty()) return true;
+        return false;
+    }
+
+    private static boolean isCommonPassword(String ps) {
+        String[] commonPasswords = {"password", "123456", "qwerty", "abc123", "111111", "iloveyou", "admin"};
+        for (String common : commonPasswords) {
+            if (ps.equalsIgnoreCase(common)) return true;
+        }
+        return false;
+    }
+
+	
 	public static String evaluator(String ps) {
 		
 		int counter = 0;
@@ -29,9 +52,13 @@ public class PasswordStrengthEvaluator {
 		if(uppercase)counter++;
 		else System.out.println("Add at least one uppercase letter.");
 		if(number)counter++;
-		else System.out.println("Include a special character for added security.");
+		else System.out.println("Increase your password length.");
 		if(special_symbol)counter++;
-		else System.out.println("Increase your password length.");		
+		else System.out.println("Include a special character for added security.");	
+		
+		if (isSequential(ps)) return "Your password is too weak: Avoid sequential characters!";
+        if (isRepeatedPattern(ps)) return "Your password is too weak: Avoid repeated patterns!";
+        if (isCommonPassword(ps)) return "Your password is too weak: Avoid common weak passwords!";
 		
 		if(counter <2)return("Your password is too weak!");
 		if(counter <5)return("Your password is secure enough");
