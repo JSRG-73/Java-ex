4591b4769c8869cc1da11c98567723b40771091c
jorge Rosas <jorge.99rg@gmail.com>
2025-01-13 23:16:59 -0600

    Roman number conversion started

--- src/exercises/RomanNumeral.java
+++ src/exercises/RomanNumeral.java
@@ -1,10 +1,21 @@
 package exercises;
 
+import java.util.Scanner;
+
 public class RomanNumeral {
+	
+	public static String toRoman(String s) {
+		
+	}
 
 	public static void main(String[] args) {
 
-
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Hello!\n\nEnter either a roman number or a normal number and I'll convert it!(From 1 to 3999)\nYour number: ");
+		String number = in.next();
+		
+		if(Character.isDigit(number.charAt(0)))
 
 	}
 
