8914be8a9cd5857cb25b54473e535af571af4ffe
jorge Rosas <jorge.99rg@gmail.com>
2025-01-25 22:48:29 -0600

    ShortestPath.java updated

--- src/exercises/ShortestPath.java
+++ src/exercises/ShortestPath.java
@@ -1,9 +1,19 @@
 package exercises;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ShortestPath {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		 Set<String> dictionary = new HashSet<>();
+	        dictionary.add("hot");
+	        dictionary.add("dot");
+	        dictionary.add("dog");
+	        dictionary.add("lot");
+	        dictionary.add("log");
+	        dictionary.add("cog");
 
 	}
 
