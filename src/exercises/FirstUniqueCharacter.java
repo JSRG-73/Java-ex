c1816a3728f61b7d6f2996edf39597bab3075480
jorge Rosas <jorge.99rg@gmail.com>
2025-02-01 22:29:57 -0600

    FirstUniqueCharacter.java updated

--- src/exercises/FirstUniqueCharacter.java
+++ src/exercises/FirstUniqueCharacter.java
@@ -1,10 +1,18 @@
 package exercises;
 
+import java.util.HashMap;
+
 public class FirstUniqueCharacter {
 
 	public static void main(String[] args) {
-		
+		String s = "loveleetcode";
+		HashMap<Character, Integer> hm = new HashMap<Character, Integer>();
 
+		for(int i=0;i<s.length();i++) {
+			if(hm.containsKey(s.charAt(i))) {
+				
+			}
+		}
 	}
 
 }
