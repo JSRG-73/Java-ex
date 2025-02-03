f402b49d1f40fc520d0515c11a91cd6e9c26a329
jorge Rosas <jorge.99rg@gmail.com>
2025-02-02 22:04:53 -0600

    FirstUniqueCharacter.java finished

--- src/exercises/FirstUniqueCharacter.java
+++ src/exercises/FirstUniqueCharacter.java
@@ -5,14 +5,19 @@ import java.util.HashMap;
 public class FirstUniqueCharacter {
 
 	public static void main(String[] args) {
-		String s = "loveleetcode";
+		String s ="character";
 		HashMap<Character, Integer> hm = new HashMap<Character, Integer>();
 
 		for(int i=0;i<s.length();i++) {
-			if(hm.containsKey(s.charAt(i))) {
-				
-			}
+			char c = s.charAt(i);
+            hm.put(c, hm.getOrDefault(c, 0) + 1);
 		}
+		
+		for (int i = 0; i < s.length(); i++) {
+            if (hm.get(s.charAt(i)) == 1) {
+                System.out.println("The first unique letter's index is " + i);
+                return;
+            }
+        }
 	}
-
 }
