0bf240437462899e0da4d7e82ecc87f7ff403e15
jorge Rosas <jorge.99rg@gmail.com>
2025-02-07 21:02:20 -0600

    MostFrequpdateduentCharacter project started

---
+++ src/exercises/MostFrequentCharacter.java
@@ -0,0 +1,21 @@
+package exercises;
+
+public class MostFrequentCharacter {
+	public static char mostFrequent(String s) {
+		char result='a';
+		int counter=0;
+		
+		for(char c='a';c<='z'; c++) {
+			
+		}
+		
+		return 'n';
+	}
+
+	public static void main(String[] args) {
+		
+
+
+	}
+
+}
