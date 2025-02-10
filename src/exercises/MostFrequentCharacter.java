385e0545bfea4bb2fed54f0a9b8d5d4b27e99c13
jorge Rosas <jorge.99rg@gmail.com>
2025-02-09 23:14:22 -0600

    MostFrequpdateduentCharacter project finished

--- src/exercises/MostFrequentCharacter.java
+++ src/exercises/MostFrequentCharacter.java
@@ -3,19 +3,24 @@ package exercises;
 public class MostFrequentCharacter {
 	public static char mostFrequent(String s) {
 		char result='a';
-		int counter=0;
+		int counter, max=0;
+		s=s.toLowerCase();
 		
 		for(char c='a';c<='z'; c++) {
+			counter=0;
+			for(char ci : s.toCharArray()) if(c==ci) counter++;
 			
+			if(counter>max) {
+				max=counter;
+				result=c;
+			}
 		}
-		
-		return 'n';
+		return result;
 	}
 
 	public static void main(String[] args) {
-		
 
-		System.out.println(mostFrequent("aabbbc"));
+		System.out.println(mostFrequent("banana"));
 	}
 
 }
