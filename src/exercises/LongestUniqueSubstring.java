0649815ddf5cbb19ad151272217e087a1bde69da
jorge Rosas <jorge.99rg@gmail.com>
2025-02-11 23:34:10 -0600

    LongestUniqueSubstring updated

--- src/exercises/LongestUniqueSubstring.java
+++ src/exercises/LongestUniqueSubstring.java
@@ -1,11 +1,21 @@
 package exercises;
 
 public class LongestUniqueSubstring {
+	
+	public static String check(String s) {
+		
+		String longest="";
+		
+		return longest;
+	}
 
 	public static void main(String[] args) {
 
 		String s = "abcabcbb";
-
+		
+		System.out.println(check(s));
+		
+		
 	}
 
 }
