cd9a0e322ea69c40cf3945dc8cba4d78c32fab37
jorge Rosas <jorge.99rg@gmail.com>
2025-02-13 23:20:08 -0600

    LongestUniqueSubstring check function updated

--- src/exercises/LongestUniqueSubstring.java
+++ src/exercises/LongestUniqueSubstring.java
@@ -1,13 +1,18 @@
 package exercises;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class LongestUniqueSubstring {
 	
 	public static String check(String s) {
 		
 		String longest="";
-		
+		Set<String> set = new HashSet<>();
 		for(int i=0;i<s.length();i++) {
-			
+	        if(set.contains(s.substring(i,i+1))) {
+	        	
+	        }
 		}
 		
 		return longest;
