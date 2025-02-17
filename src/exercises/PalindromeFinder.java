9ca0123c4954bfb5ffad06ad420601d6ef3832d6
jorge Rosas <jorge.99rg@gmail.com>
2025-02-16 23:12:43 -0600

    PalindromeFinder updated

--- src/exercises/PalindromeFinder.java
+++ src/exercises/PalindromeFinder.java
@@ -1,6 +1,12 @@
 package exercises;
 
 public class PalindromeFinder {
+	
+	public static String finder(String s) {
+		String result="";
+		
+		return result;
+	}
 
 	public static void main(String[] args) {
 
