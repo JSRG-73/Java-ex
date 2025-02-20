dc42728906fba144ffba97db470eb6cb33580be6
jorge Rosas <jorge.99rg@gmail.com>
2025-02-19 23:34:16 -0600

    SmallestWindow updated

--- src/exercises/SmallestWindow.java
+++ src/exercises/SmallestWindow.java
@@ -1,10 +1,26 @@
 package exercises;
 
 public class SmallestWindow {
+	
+	public String find(String s, String t, int window) {
+		
+		
+		
+		return "";
+	}
 
 	public static void main(String[] args) {
 		
-
+		String s = "ADOBECODEBANC", t = "ABC";
+		
+		SmallestWindow sw = new SmallestWindow();
+		
+		for (int i=t.length(); i<s.length();i++) {
+			if(!sw.find(s, t, i).equals("")) {
+				System.out.println(sw.find(s, t, i));
+				break;
+			}
+		}
 	}
 
 }
