b156c3bd657ba25781e5b96a8e48fe4ada220e3d
jorge Rosas <jorge.99rg@gmail.com>
2025-02-23 23:27:59 -0600

    LongestUniformString updated

--- src/exercises/LongestUniformString.java
+++ src/exercises/LongestUniformString.java
@@ -1,6 +1,13 @@
 package exercises;
 
 public class LongestUniformString {
+	
+	public static int  findlongest(String s, String t) {
+		
+		int result=0;
+		
+		return result;
+	}
 
 	public static void main(String[] args) {
 		
